80c59d027f9ba8b8c4136b6e612d31971c6bc7b8
adoerr <0xad@gmx.net>
2023-02-04 16:43:08 +0100

    list plugin builders
    Signed-off-by: adoerr <0xad@gmx.net>

--- examples/pako-analyzer/src/main.rs
+++ examples/pako-analyzer/src/main.rs
@@ -1,7 +1,8 @@
 #![warn(clippy::all)]
 
 use anyhow::Result;
-use clap::{Parser, Subcommand};
+use clap::{command, Parser, Subcommand};
+use pako_core::plugins::PluginsFactory;
 
 #[derive(Parser, Debug)]
 #[command(version, about = "Pako Demo Analyzer")]
@@ -11,10 +12,24 @@ struct Cli {
 }
 
 #[derive(Subcommand, Debug)]
-enum Commands {}
+enum Commands {
+    /// List plugin builders
+    Builders,
+}
 
 fn main() -> Result<()> {
     env_logger::try_init()?;
+
+    let cli = Cli::parse();
+
+    let factory = PluginsFactory::default();
+
+    match cli.command {
+        Commands::Builders => {
+            println!("Plugin builders:");
+            factory.iter_builders(|b| println!("    {b}"));
+        }
+    }
 
     Ok(())
 }
