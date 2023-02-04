b88005c6178b3642932dfa4d3065dbdcf5acb2b8
adoerr <0xad@gmx.net>
2023-02-04 17:19:02 +0100

    list available plugins
    Signed-off-by: adoerr <0xad@gmx.net>

--- examples/pako-analyzer/src/main.rs
+++ examples/pako-analyzer/src/main.rs
@@ -2,7 +2,11 @@
 
 use anyhow::Result;
 use clap::{command, Parser, Subcommand};
-use pako_core::plugins::PluginsFactory;
+use pako_core::{
+    plugins::PluginsFactory, Plugin, PLUGIN_FLOW_DEL, PLUGIN_FLOW_NEW, PLUGIN_L2, PLUGIN_L3,
+    PLUGIN_L4,
+};
+use pako_tools::Config;
 
 #[derive(Parser, Debug)]
 #[command(version, about = "Pako Demo Analyzer")]
@@ -15,6 +19,8 @@ struct Cli {
 enum Commands {
     /// List plugin builders
     Builders,
+    /// List available plugins
+    Plugins,
 }
 
 fn main() -> Result<()> {
@@ -23,11 +29,41 @@ fn main() -> Result<()> {
     let cli = Cli::parse();
 
     let factory = PluginsFactory::default();
+    let registry = factory.build_plugins(&Config::default())?;
+
+    let plugins = |p: &mut dyn Plugin| {
+        println!("  {}", p.name());
+
+        let t = p.plugin_type();
+        print!("    layers:");
+        if t & PLUGIN_L2 != 0 {
+            print!("  L2");
+        }
+        if t & PLUGIN_L3 != 0 {
+            print!("  L3");
+        }
+        if t & PLUGIN_L4 != 0 {
+            print!("  L4");
+        }
+        println!();
+
+        print!("    events:");
+        if t & PLUGIN_FLOW_NEW != 0 {
+            print!("  FLOW_NEW");
+        }
+        if t & PLUGIN_FLOW_DEL != 0 {
+            print!("  FLOW_DEL")
+        }
+        println!()
+    };
 
     match cli.command {
         Commands::Builders => {
             println!("Plugin builders:");
             factory.iter_builders(|b| println!("    {b}"));
+        }
+        Commands::Plugins => {
+            registry.run_plugins(|_| true, plugins);
         }
     }
 
