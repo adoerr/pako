e752b26b70309501c345f4053f84a24e58b4c92b
adoerr <0xad@gmx.net>
2023-02-16 14:01:30 +0100

    ByteOrder trait
    Signed-off-by: adoerr <0xad@gmx.net>

--- pako-pcap/src/endian.rs
+++ pako-pcap/src/endian.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use nom::{error::ParseError, IResult};
+
 pub enum BigEndian {}
 
 /// System native endianness
@@ -14,3 +16,9 @@ pub type NativeEndian = LittleEndian;
 
 /// Network byte order
 pub type NetworkEndian = BigEndian;
+
+/// [`ByteOrder`] can deserialize integers from bytes
+pub(crate) trait ByteOrder {
+    fn parse_u16<'a, E: ParseError<&'a [u8]>>(input: &'a [u8]) -> IResult<&'a [u8], u16, E>;
+    fn parse_u32<'a, E: ParseError<&'a [u8]>>(input: &'a [u8]) -> IResult<&'a [u8], u32, E>;
+}
