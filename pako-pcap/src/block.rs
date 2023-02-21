1929c9892a20d0d4cefacf2b2448376c0145b0fb
adoerr <0xad@gmx.net>
2023-02-21 07:17:23 +0100

    add block types
    Signed-off-by: adoerr <0xad@gmx.net>

--- pako-pcap/src/block.rs
+++ pako-pcap/src/block.rs
@@ -1,10 +1,32 @@
 //!
 //! General Block Structure support
 //!
+//! A capture file is organized in blocks, that are appended one to another
+//! to form the file. All the blocks share a common format,
+//!
+//!     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//!    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//!  0 |                          Block Type                           |
+//!    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//!  4 |                      Block Total Length                       |
+//!    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//!  8 /                          Block Body                           /
+//!    /              variable length, padded to 32 bits               /
+//!    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//!    |                      Block Total Length                       |
+//!    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 /// The block type uniquely identifies a block.
 #[repr(u32)]
 #[derive(Debug)]
 pub enum BlockType {
     SectionHeader = 0x0A0D_0D0A,
+    InterfaceDescription = 0x0000_0001,
+    SimplePacket = 0x0000_0003,
+    NameResolution = 0x0000_0004,
+    InterfaceStatistic = 0x0000_0005,
+    EnhancedPacket = 0x0000_0006,
+    SystemJournalExport = 0x0000_0009,
+    DecryptionSecretsBlock = 0x0000_000A,
+    Custom = 0x0000_0BAD,
 }
